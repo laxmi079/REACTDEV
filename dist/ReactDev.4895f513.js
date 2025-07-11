d7374bf2ab0e031055dd1a1fadc53e2f11501657
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-07-11 17:40:31 +0530

    episode2

--- dist/ReactDev.4895f513.js
+++
@@ -1,2 +0,0 @@
-let parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])]),root=ReactDOM.createRoot(document.getElementById("root"));root.render(parent);
-//# sourceMappingURL=ReactDev.4895f513.js.map
