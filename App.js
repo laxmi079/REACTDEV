484c0c9cfde514dfd35e3787fb1c2e0165ec0fd7
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-07-11 17:37:13 +0530

    episode2

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-        
-        //  <div id="parent">
-        //     <div id="child">
-        //         <h1>I am h1</h1>
-        //         <h2>I am h2</h2>
-        //     </div>
-        //  </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-         const parent=React.createElement("div",{id:"parent"},
-            [React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
-            React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]) ]);
-        
-        // const heading=React.createElement("h1",
-        //     {id:"heading" , xyz:"abc"},"hello form react");
-        const root=ReactDOM.createRoot(document.getElementById("root"));
-        root.render(parent);
+// Build children as array with keys
+const childrenArray = [
+  React.createElement("div", { id: "child", key: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+];
+
+const parent = React.createElement("div", { id: "parent" }, childrenArray);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
