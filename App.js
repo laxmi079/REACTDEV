61866de742316b8a2b6ab4b4f1a0237744afc209
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-07-11 13:26:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+        
+        //  <div id="parent">
+        //     <div id="child">
+        //         <h1>I am h1</h1>
+        //         <h2>I am h2</h2>
+        //     </div>
+        //  </div>
+
+         const parent=React.createElement("div",{id:"parent"},
+            [React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
+            React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]) ]);
+        
+        // const heading=React.createElement("h1",
+        //     {id:"heading" , xyz:"abc"},"hello form react");
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+        root.render(parent);
